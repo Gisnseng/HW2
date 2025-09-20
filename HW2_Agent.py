12cff1ff5e20c55f4d86e3fa6e08019a4c17f539
Indiana <atwoodi26@up.edu>
2025-09-20 14:17:48 -0700

    Updates methods for HW2 (per description), but utility() is still very buggy

--- HW2_Agent.py
+++ HW2_Agent.py
@@ -8,26 +8,40 @@ from Ant import UNIT_STATS
 from Move import Move
 from GameState import *
 from AIPlayerUtils import *
+import pdb
 
+##############################
+###
+###   Helper functions for HW2b
+###
+##############################
 
-##
-#AIPlayer
-#Description: The responsbility of this class is to interact with the game by
-#deciding a valid move based on a given game state. This class has methods that
-#will be implemented by students in Dr. Nuxoll's AI course.
-#
-#Variables:
-#   playerId - The id of the player.
-##
+# Used to create root node and subsequent child nodes
+def createNode(move, state, depth, parent):
+    node = {
+        "move": move,
+        "state": state,
+        "depth": depth,
+        "parent": parent,
+        "evaluation": utility(state) + depth
+        # Evalution is the sum of the utility of the state and it's depth
+    }
+
+    return node
 
 # Determines the distance between selected Ant and its target
 def getDistance(a, b):
-
     # Returns a distance between point A and point B's coordinates
     a_X, a_Y = a.coords
     b_X, b_Y = b.coords
 
     return abs(a_X - b_X) + abs(a_Y - b_Y)
+
+##############################
+###
+###   Required functions for HW2b
+###
+##############################
 
 # Determines the best course of action (most successful Move)
 def utility(state):
@@ -66,7 +80,7 @@ def utility(state):
         if worker.carrying:
             # distance to tunnel only
             total_distance = getDistance(worker, tunnel)
-            print(f"WORKER COORDS: {worker.coords}")
+            # print(f"WORKER COORDS: {worker.coords}")
 
             # Issue here: it seems that the coords to move to the tunnel are never even passed to utility
         else:
@@ -110,21 +124,52 @@ def utility(state):
     # print(f"TOTAL: {total_moves_needed}")
     return total_moves_needed
 
+## EXPAND NODE
+def expandNode(currentNode):
+    # Get the state at the current node, list all legal moves
+    currentState = currentNode["state"]
+    legal_moves = listAllLegalMoves(currentState)
+    node_list = []
 
-def bestMove(nodes): #find best move in a given list of nodes
+    # Get next state for each move, increment the depth from the current node
+    for move in legal_moves:
+        nextState = getNextState(currentState, move)
+        depth = currentNode["depth"] + 1
+
+        expandedNode = createNode(move, nextState, depth, currentNode)
+
+        node_list.append(expandedNode)
+
+    # Return nodes of next depth
+    return node_list
+
+## BEST MOVE
+def bestNode(nodes):
+    # Find best node in a given list of nodes
     least_utility = 100
-    best_move = None
+    best_node = None
 
+    # Find the smallest utility and return the associated node
     for node in nodes:
         utility = node["evaluation"]
-        move = node["move"]
 
-        if (utility < least_utility): # rank their utility and take the best
+        # Rank their utility and take the best
+        if (utility < least_utility):
             least_utility = utility
-            best_move = move
+            best_node = node
 
-    return best_move
+    return best_node
 
+
+##
+#AIPlayer
+#Description: The responsbility of this class is to interact with the game by
+#deciding a valid move based on a given game state. This class has methods that
+#will be implemented by students in Dr. Nuxoll's AI course.
+#
+#Variables:
+#   playerId - The id of the player.
+##
 class AIPlayer(Player):
 
     #__init__
@@ -200,27 +245,45 @@ class AIPlayer(Player):
     #   currentState - The state of the current game waiting for the player's move (GameState)
     #
     #Return: The Move to be made
-    ##    
-
+    ##
     def getMove(self, currentState):
-        legal_moves = listAllLegalMoves(currentState)
-        node_list = []
+        # Create lists for frontier and expanded nodes
+        frontierNodes = []
+        expandedNodes = []
 
-        for move in legal_moves:
-            nextState = getNextState(currentState, move)
-            depth = 1   # depth stays 1 for HW A
+        # Root node has no move, no depth, and no parent node
+        rootNode = createNode(None, currentState, 0, None)
 
-            node = {
-                "move": move,
-                "state": nextState,
-                "depth": depth,
-                "parent": currentState,
-                "evaluation": utility(nextState) + depth
-                # evaluation is the sum of the utility and the depth
-            }
-            node_list.append(node)
+        # Add root node to frontierNodes
+        frontierNodes.append(rootNode)
 
-        return bestMove(node_list)
+        # Iterates to depth 3
+        iterations = 0
+        while iterations < 3:
+            # Find the best utility in current frontier nodes
+            # Remove from frontier nodes, and add to expanded nodes
+            bestNodeChoice = bestNode(frontierNodes)
+            for node in frontierNodes:
+                if node == bestNodeChoice:   # had to do this to compare data, not ObjectIDs
+                    frontierNodes.remove(node)
+            expandedNodes.append(bestNodeChoice)
+
+            # Expand the best node and add new nodes to frontier
+            newNodes = expandNode(bestNodeChoice)
+            frontierNodes.extend(newNodes)
+
+            iterations += 1
+
+        # Chose the best node out of all frontier nodes
+        # Go back to parent (at depth == 1), and return the associated move
+        chosenNode = bestNode(frontierNodes)
+        while True:
+            if chosenNode["depth"] == 1:
+                break
+            else:
+                chosenNode = chosenNode["parent"]
+        
+        return chosenNode["move"]
 
     
     ##
@@ -248,28 +311,28 @@ class AIPlayer(Player):
 # Unit tests
 
 # utility
-blank_state = GameState.getBasicState()
-blank_state_next = getNextState(blank_state, listAllLegalMoves(blank_state)[0])
-print(utility(blank_state_next))
-#This returns zero because the test state doesn't have any food
+# blank_state = GameState.getBasicState()
+# blank_state_next = getNextState(blank_state, listAllLegalMoves(blank_state)[0])
+# print(utility(blank_state_next))
+# #This returns zero because the test state doesn't have any food
 
-# bestMove
-legal_moves = listAllLegalMoves(blank_state)
-node_list = []
+# # bestMove
+# legal_moves = listAllLegalMoves(blank_state)
+# node_list = []
 
-for move in legal_moves:
-    nextState = getNextState(blank_state, move)
-    depth = 1   
+# for move in legal_moves:
+#     nextState = getNextState(blank_state, move)
+#     depth = 1   
 
-    node = {
-        "move": move,
-        "state": nextState,
-        "depth": depth,
-        "parent": blank_state,
-        "evaluation": utility(nextState) + depth
+#     node = {
+#         "move": move,
+#         "state": nextState,
+#         "depth": depth,
+#         "parent": blank_state,
+#         "evaluation": utility(nextState) + depth
         
-    }
-    node_list.append(node)
-print(bestMove(node_list))
-# This prints the move action that moves the ant at 0,0 to 1,0
-# getMove is omitted because it does the exact same thing we did to test bestMove
+#     }
+#     node_list.append(node)
+# print(bestMove(node_list))
+# # This prints the move action that moves the ant at 0,0 to 1,0
+# # getMove is omitted because it does the exact same thing we did to test bestMove
